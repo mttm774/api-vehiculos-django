2a61f355112aa9e8fd013f8ed0973166d81a052d
migueltovar <loganitsolutions@outlook.com>
2024-05-24 00:00:16 -0500

    ajustando urls

--- parking/vehicle_management/urls.py
+++ parking/vehicle_management/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from .views import VehicleApiView, VehicleQueryApiView
 
 urlpatterns = [
+    path ('crear-vehiculo',VehicleApiView.as_view()),
     path ('list',VehicleApiView.as_view()),
-    path ('list/<int:pkid>/',VehicleApiView.as_view(),name='actualizar_vehiculo'),
+    path ('actualizar-vehiculo/<int:pkid>',VehicleApiView.as_view(),name='actualizar_vehiculo'),
     path ('consultar/<int:id>/',VehicleQueryApiView.as_view(),name='obtener_vehiculo')
     
 ]
